ac7859dc58e3a2b3b3b4394b16519713b222d057
e.murij <murij.kat@gmail.com>
2022-04-07 20:53:37 +0700

    Created project, added requirements.txt, gitignore update

---
+++ SENT_HABR/SENT_HABR/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SENT_HABR project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SENT_HABR.settings')
+
+application = get_asgi_application()
