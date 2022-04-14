dcb810c48819dcb926bb6a6199b463c190ac0480
yarikmik <yarik.mik@gmail.com>
2022-04-14 20:22:17 +0700

    основное меню сайдбары, футер блок контента выведены в шаблоны джанго

--- SENT_HABR/mainapp/views.py
+++ SENT_HABR/mainapp/views.py
@@ -7,6 +7,26 @@ class IndexListView(ListView):
     model = Article
 
 
+class DesignListView(ListView):
+    template_name = 'mainapp/design.html'
+    model = Article
+
+
+class WebDevelopmentListView(ListView):
+    template_name = 'mainapp/web_development.html'
+    model = Article
+
+
+class MobileDevelopmentListView(ListView):
+    template_name = 'mainapp/mobile_development.html'
+    model = Article
+
+
+class MarketingListView(ListView):
+    template_name = 'mainapp/marketing.html'
+    model = Article
+
+
 class HelpListView(ListView):
     template_name = 'mainapp/help.html'
     model = Article
