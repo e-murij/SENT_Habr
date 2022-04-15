6817116c2606dc88488b1da2b34c2509524e2ba1
Egor Taran <64767049+egortaran@users.noreply.github.com>
2022-04-15 13:01:07 +0600

    exclude
    Видимые поля: tags, section, content
    Невидимые поля: author, is_active, is_published

--- SENT_HABR/articleapp/forms.py
+++ SENT_HABR/articleapp/forms.py
@@ -7,6 +7,7 @@ class ArticleCreateForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = '__all__'
+        exclude = ('author', 'is_active', 'is_published',)
 
     # Всем полям формы добавляется значение 'form-control' http-атрибута 'class'
     def __init__(self, *args, **kwargs):
