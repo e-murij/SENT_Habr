11faa5107fe21240b67374d1a0cf6da80b7121a9
yarikmik <yarik.mik@gmail.com>
2022-04-15 18:49:00 +0700

    Удалил лишние вьюшки и страницы

--- SENT_HABR/mainapp/views.py
+++ SENT_HABR/mainapp/views.py
@@ -12,56 +12,6 @@ class IndexListView(ListView):
         return context
 
 
-class DesignListView(ListView):
-    template_name = 'mainapp/design.html'
-    model = Article
-
-    def get_context_data(self, **kwargs):
-        context = super(DesignListView, self).get_context_data(**kwargs)
-        context['title'] = 'Design'
-        return context
-
-
-class WebDevelopmentListView(ListView):
-    template_name = 'mainapp/web_development.html'
-    model = Article
-
-    def get_context_data(self, **kwargs):
-        context = super(WebDevelopmentListView, self).get_context_data(**kwargs)
-        context['title'] = 'Web Development'
-        return context
-
-
-class MobileDevelopmentListView(ListView):
-    template_name = 'mainapp/mobile_development.html'
-    model = Article
-
-    def get_context_data(self, **kwargs):
-        context = super(MobileDevelopmentListView, self).get_context_data(**kwargs)
-        context['title'] = 'Mobile Development'
-        return context
-
-
-class MarketingListView(ListView):
-    template_name = 'mainapp/marketing.html'
-    model = Article
-
-    def get_context_data(self, **kwargs):
-        context = super(MarketingListView, self).get_context_data(**kwargs)
-        context['title'] = 'Marketing'
-        return context
-
-
-class HelpListView(ListView):
-    template_name = 'mainapp/help.html'
-    model = Article
-
-    def get_context_data(self, **kwargs):
-        context = super(HelpListView, self).get_context_data(**kwargs)
-        context['title'] = 'Help'
-        return context
-
-
 class SectionListView(ListView):
     template_name = 'mainapp/index.html'
     model = Article
