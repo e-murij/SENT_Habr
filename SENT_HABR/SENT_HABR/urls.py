2478b105534ae0e0a6218f63426f680e2f0d9f5c
e.murij <murij.kat@gmail.com>
2022-04-20 17:35:48 +0700

    add ckeditor

--- SENT_HABR/SENT_HABR/urls.py
+++ SENT_HABR/SENT_HABR/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.views.decorators.cache import never_cache
+from ckeditor_uploader import views as ckeditor_views
+
+from SENT_HABR import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
+    path('ckeditor/upload/', login_required(ckeditor_views.upload), name='ckeditor_upload'),
+    path('ckeditor/browse/', never_cache(login_required(ckeditor_views.browse)), name='ckeditor_browse'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
