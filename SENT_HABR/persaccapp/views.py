8eccbbe8f04f214ad3b8e027be70fc424f053f86
e.murij <murij.kat@gmail.com>
2022-04-21 22:43:28 +0700

    add PersonalDetailView and correct register_form

--- SENT_HABR/persaccapp/views.py
+++ SENT_HABR/persaccapp/views.py
@@ -1,5 +1,4 @@
 from django.views.generic import DetailView
-
 from authapp.models import User
 
 
@@ -7,3 +6,7 @@ class PersonalDetailView(DetailView):
     template_name = 'persaccapp/personal_page.html'
     model = User
 
+    def get_context_data(self, **kwargs):
+        context = super(PersonalDetailView, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
