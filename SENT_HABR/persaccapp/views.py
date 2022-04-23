f619b7dec73e413f9a30f2e1f0b0d0042210dbf5
e.murij <murij.kat@gmail.com>
2022-04-23 17:52:41 +0700

    add my_articles

--- SENT_HABR/persaccapp/views.py
+++ SENT_HABR/persaccapp/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, ListView
+
+from articleapp.models import Article
 from authapp.models import User
+from persaccapp.services import get_articles_by_author
 
 
 class PersonalDetailView(DetailView):
@@ -9,4 +13,18 @@ class PersonalDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(PersonalDetailView, self).get_context_data(**kwargs)
         context['user'] = self.request.user
+        return context
+
+
+class MyArticlesList(LoginRequiredMixin, ListView):
+    template_name = 'persaccapp/my_articles.html'
+    model = Article
+
+    def get_queryset(self):
+        queryset = super(MyArticlesList, self).get_queryset()
+        return get_articles_by_author(queryset, self.request.user.pk)
+
+    def get_context_data(self, **kwargs):
+        context = super(MyArticlesList, self).get_context_data(**kwargs)
+        context['title'] = 'My Articles'
         return context
