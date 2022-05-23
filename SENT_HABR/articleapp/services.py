1d177e8466f135e9b360bf385bc32bb0b88e3ddc
e.murij <murij.kat@gmail.com>
2022-05-23 22:30:11 +0700

    add popular users and articles

--- SENT_HABR/articleapp/services.py
+++ SENT_HABR/articleapp/services.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
-
 from articleapp.models import Article
 
 
 def get_article_by_id(article_id):
+    """Возвращает статью с id = article_id """
     article = get_object_or_404(Article, pk=article_id)
     return article
 
@@ -21,3 +21,4 @@ def get_articles_by_section(queryset, section_slug):
 def get_comments(article):
     """Возвращает все комментарии к статье article"""
     return article.comment_set.filter(parent__isnull=True)
+
--- SENT_HABR/mainapp/context_processors.py
+++ SENT_HABR/mainapp/context_processors.py
@@ -1,8 +1,22 @@
-from articleapp.models import Section
+from articleapp.models import Section, Article
+from articleapp.services import get_all_articles
+from authapp.models import User
+from likeapp.services import get_popular_elements
 
 
 def sections(request):
+    """Передача разделов сайта в контекст"""
     sections_menu = Section.objects.all()
     return {
         'sections_menu': sections_menu,
+    }
+
+
+def popular_articles_and_users(request):
+    """Передача наиболее популярных пользователей и статей  в контекст"""
+    popular_articles = get_popular_elements(get_all_articles(Article.objects.all()), 5)
+    popular_users = get_popular_elements(User.objects.all().filter(is_active=True), 5)
+    return {
+        'popular_articles': popular_articles,
+        'popular_users': popular_users,
     }
