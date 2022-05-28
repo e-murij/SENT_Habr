aae91104fc9fb11bdb0eb3645d695c39658ed2e8
e.murij <murij.kat@gmail.com>
2022-05-28 23:53:31 +0700

    added search

--- SENT_HABR/SENT_HABR/settings.py
+++ SENT_HABR/SENT_HABR/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'commentapp',
     'likeapp',
     'notificationapp',
+    'searchapp',
 
 ]
 
--- SENT_HABR/mainapp/urls.py
+++ SENT_HABR/mainapp/urls.py
@@ -5,11 +5,12 @@ from .views import IndexListView, SectionListView, HelpView
 urlpatterns = [
     path('', IndexListView.as_view(), name='index'),
     path('help/', HelpView.as_view(), name='help'),
-    path('<slug:section_slug>/', SectionListView.as_view(), name='section'),
     path('auth/', include('authapp.urls', namespace='auth'), name='auth'),
     path('article/', include('articleapp.urls', namespace='article'), name='article'),
     path('account/', include('persaccapp.urls', namespace='account'), name='account'),
     path('comment/', include('commentapp.urls', namespace='comment'), name='comment'),
     path('likes/', include('likeapp.urls', namespace='likes'), name='likes'),
     path('notification/', include('notificationapp.urls', namespace='notification'), name='notification'),
+    path('search/', include('searchapp.urls', namespace='search'), name='search'),
+    path('<slug:section_slug>/', SectionListView.as_view(), name='section'),
 ]
