018c30ecc5a1f12983a3c290ef26381652c54932
e.murij <murij.kat@gmail.com>
2022-06-07 16:02:35 +0700

    change tests.py

--- SENT_HABR/authapp/tests.py
+++ SENT_HABR/authapp/tests.py
@@ -11,32 +11,50 @@ class TestUserManagement(TestCase):
         self.client = Client()
 
         self.superuser = User.objects.create_superuser('Admin_test', \
-                                                       'test_admin@test.local', 'Qwerty1')
+                                                       'test_admin@test.local', 'Qwerty1', is_verify=True)
 
-        self.user = User.objects.create_user('user_test1', 'user_test1@test.local', 'Qwerty1')
+        self.user = User.objects.create_user('user_test1', 'user_test1@test.local', 'Qwerty1', is_verify=True)
 
         self.user_with__first_name = User.objects.create_user('user_test2', 'user_test2@test.local', 'Qwerty1', \
                                                               first_name='Test2')
 
     def test_user_login(self):
-        # главная без логина
+        # пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_anonymous)
 
-        # данные пользователя
         self.client.login(username='user_test1', password='Qwerty1')
 
-        # логинимся
         response = self.client.get('/auth/login/')
         self.assertFalse(response.context['user'].is_anonymous)
         self.assertEqual(response.context['user'], self.user)
 
-        # главная после логина
+        # пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['user'], self.user)
 
-    def tearDown(self):
-        call_command('sqlsequencereset', 'mainapp', 'authapp', \
-                     'articleapp', 'likeapp', 'commentapp', 'notificationapp', 'persaccapp')
+    def test_user_access(self):
+        self.client.login(username='user_test1', password='Qwerty1')
+        response = self.client.get('/admin/index')
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.context['user'], self.user)
+
+    #
+    #     # пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    #     self.client.login(username='Admin_test', password='Qwerty1')
+    #     response = self.client.get('/admin/')
+    #     self.assertEqual(response.context['user'], self.user)
+
+    # def tearDown(self):
+    #     call_command('sqlsequencereset',
+    #                  'mainapp',
+    #                  'articleapp',
+    #                  'persaccapp',
+    #                  'authapp',
+    #                  'commentapp',
+    #                  'likeapp',
+    #                  'notificationapp',
+    #                  'searchapp',
+    #                  )
